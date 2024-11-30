619d2448f049aef41ed8592764a587b177b76d46
Nikita Krenov <nikitabaram@gmail.com>
2024-11-30 14:47:15 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Imitation
+{
+    public class Animal
+    {
+        private string? _name;
+        private int? _age;
+        private string? _type;
+
+        public string Name
+        {
+            get => _name ?? "No name";
+            set => _name = value;
+        }
+
+        public int Age
+        {
+            get => _age ?? -1;
+            set => _age = value;
+        }
+
+        public string Type
+        {
+            get => _type ?? "No type";
+            set => _type = value;
+        }
+
+        public Animal(string? name, int? age, string? type)
+        {
+            Name = name;
+            Age = age ?? -1; // Fix: Use null-coalescing operator to provide a default value
+            Type = type;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello, World!: ");
+            Console.Read();
+        }
+    }
+}
